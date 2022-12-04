29a57ecfe66e754121225e6781afcd04f21054b4
Abhiram <a4abhiram125@gmail.com>
2022-12-04 17:39:29 +0530

    updates index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 const library = [];
 
 const addBookBtn = document.getElementById("add-book");
-const bookContainer = document.getElementsByClassName("book-container");
+const books = document.getElementById("books");
 const inputBook = document.getElementsByClassName("input-book");
 
 class Book {
-  constructor(title, author, pages, haveRead) {
+  constructor(title, author, pages, position) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.haveRead = haveRead;
+    this.position = position;
   }
 }
 
@@ -24,13 +24,9 @@ addBookBtn.addEventListener("click", function () {
     alert("please Enter all The Details.");
   } else {
     library.push(
-      new Book(
-        inputBook[0].value,
-        inputBook[1].value,
-        inputBook[2].value,
-        (haveRead = true)
-      )
+      new Book(inputBook[0].value, inputBook[1].value, inputBook[2].value)
     );
+    addBookBtn.addEventListener("click", addBook(library.length - 1));
   }
   inputBook[0].value = "";
   inputBook[1].value = "";
@@ -41,55 +37,67 @@ addBookBtn.addEventListener("click", function () {
 function addBook(arrayIndex) {
   let book = document.createElement("div");
   book.classList.add("book");
-  book.innerText = "Lorem100";
+
   let ul = document.createElement("ul");
-  for (let i = 0; i < inputBook.length; i++) {
-    let li = document.createElement("li");
-    li.innerText = "Book Name :" + `${library[arrayIndex].title}`;
-    ul.appendChild(li);
-    li.innerText = "Auther Name:" + `${library[arrayIndex].author}`;
-    ul.appendChild(li);
-    li.innerText = "Number of Pages :" + `${library[arrayIndex].pages}`;
-    ul.appendChild(li);
-  }
+
+  let li = document.createElement("li");
+  li.innerText = "Title : " + library[arrayIndex].title;
+  ul.appendChild(li);
+
+  let li2 = document.createElement("li");
+  li2.innerText = "Author : " + library[arrayIndex].author;
+  ul.appendChild(li2);
+
+  let li3 = document.createElement("li");
+  li3.innerText = "Pages : " + library[arrayIndex].pages;
+  ul.appendChild(li3);
+
   book.appendChild(ul);
 
   let buttons = document.createElement("div");
   buttons.classList.add("button-js-container");
   let status = document.createElement("button");
   status.classList.add("status");
-  if (haveRead === true) {
-    status.innerText = "Read";
-    status.style.backgroundColor = "#9fff9c";
-  } else {
-    status.innerText = "Not Read";
-    status.style.backgroundColor = "red";
-  }
+  status.innerText = "Read";
   status.addEventListener("click", function () {
     if (status.innerText === "Read") {
       status.innerText = "Not Read";
-      status.style.backgroundColor = "red";
+      status.style.backgroundColor = "rgb(255, 156, 156)";
     } else {
       status.innerText = "Read";
       status.style.backgroundColor = "#9fff9c";
     }
   });
-
+  for (let i = 0; i < library.length; i++) {
+    library[i].position = i;
+  }
   let deleteButton = document.createElement("button");
   deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`;
-  deleteButton.classList.add("deleteBook");
-  deleteButton.addEventListener("click", function (e) {
-    let target = e.target;
-    target.parentElement.parentElement.remove();
+  deleteButton.classList.add("delete-book");
+  deleteButton.addEventListener("click", function () {
+    if (
+      deleteButton.parentElement.parentElement.position ===
+      library.length - 1
+    ) {
+      library.shift();
+    } else if (deleteButton.parentElement.parentElement.position === 0) {
+      library.pop();
+    } else {
+      delete library[deleteButton.parentElement.parentElement.position];
+      fillEmptySpace(deleteButton.parentElement.parentElement.position);
+    }
+    deleteButton.parentElement.parentElement.remove();
   });
-
   buttons.appendChild(status);
   buttons.appendChild(deleteButton);
-  book.append(buttons);
-  console.log(book);
-  return book;
+  book.appendChild(buttons);
+  books.appendChild(book);
 }
-console.log(bookContainer);
-for (let i = 0; i < bookContainer.length; i++) {
-  console.log(bookContainer[i]);
+
+/** function to shift elements to lower positions after deleting at random position */
+function fillEmptySpace(index) {
+  for (let i = index; i < library.length; i++) {
+    library[i] = library[i + 1];
+  }
+  library.shift();
 }
